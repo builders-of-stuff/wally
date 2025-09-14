2b3b642010210ced08c60ce23e948d342cd7c48f
Kyle Truong <ktruong008@gmail.com>
2025-09-14 16:29:26 -0400

    init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+  import {
+    ConnectButton,
+    devnetWalletAdapter as walletAdapter
+  } from '@builders-of-stuff/svelte-sui-wallet-adapter';
+</script>
+
+<div class="">
+  <ConnectButton {walletAdapter} />
+</div>
+
+<h1 class=" ">Welcome to SvelteKit</h1>
 <p>
   Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
 </p>
